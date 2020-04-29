a765d8d367b0d18265edb4f79f9d664d220ea6fd
Henry Zhu <hi@henryzoo.com>
2020-04-29 15:57:05 -0400

    format

--- app/config.js
+++ app/config.js
@@ -1,68 +1,80 @@
-var nconf = require('nconf').argv().env().file({ file:'config.json' });
+var nconf = require("nconf").argv().env().file({ file: "config.json" });
 
 // List of commands to check for
 var commands = [
-    'left', 'right', 'up', 'down',
-    'start', 'select',
-    'a', 'b',
-    'democracy', 'anarchy'
+  "left",
+  "right",
+  "up",
+  "down",
+  "start",
+  "select",
+  "a",
+  "b",
+  "democracy",
+  "anarchy",
 ];
 
-var username = process.env.TWITCH_USERNAME || nconf.get('TWITCH_USERNAME');
-var oauth = process.env.TWITCH_OAUTH || nconf.get('TWITCH_OAUTH');
-var channel = process.env.TWITCH_CHANNEL || nconf.get('TWITCH_CHANNEL');
-var os = process.env.CONFIG_OS || nconf.get('CONFIG_OS');
-var programName = process.env.CONFIG_PROGRAM_NAME || nconf.get('CONFIG_PROGRAM_NAME');
-var maxCharName = process.env.CONFIG_MAX_CHAR_NAME || nconf.get('CONFIG_MAX_CHAR_NAME');
-var maxCharCommand = process.env.CONFIG_MAX_CHAR_COMMAND || nconf.get('CONFIG_MAX_CHAR_COMMAND');
-var sendKey = process.env.CONFIG_SEND_KEY || nconf.get('CONFIG_SEND_KEY');
-var serverIP = process.env.TWITCH_IP || nconf.get('TWITCH_IP');
-var filteredCommands = process.env.CONFIG_FILTERED_COMMANDS || nconf.get('CONFIG_FILTERED_COMMANDS');
-var throttledCommands = process.env.CONFIG_THROTTLED_COMMANDS || nconf.get('CONFIG_THROTTLED_COMMANDS');
+var username = process.env.TWITCH_USERNAME || nconf.get("TWITCH_USERNAME");
+var oauth = process.env.TWITCH_OAUTH || nconf.get("TWITCH_OAUTH");
+var channel = process.env.TWITCH_CHANNEL || nconf.get("TWITCH_CHANNEL");
+var os = process.env.CONFIG_OS || nconf.get("CONFIG_OS");
+var programName =
+  process.env.CONFIG_PROGRAM_NAME || nconf.get("CONFIG_PROGRAM_NAME");
+var maxCharName =
+  process.env.CONFIG_MAX_CHAR_NAME || nconf.get("CONFIG_MAX_CHAR_NAME");
+var maxCharCommand =
+  process.env.CONFIG_MAX_CHAR_COMMAND || nconf.get("CONFIG_MAX_CHAR_COMMAND");
+var sendKey = process.env.CONFIG_SEND_KEY || nconf.get("CONFIG_SEND_KEY");
+var serverIP = process.env.TWITCH_IP || nconf.get("TWITCH_IP");
+var filteredCommands =
+  process.env.CONFIG_FILTERED_COMMANDS || nconf.get("CONFIG_FILTERED_COMMANDS");
+var throttledCommands =
+  process.env.CONFIG_THROTTLED_COMMANDS ||
+  nconf.get("CONFIG_THROTTLED_COMMANDS");
 
 var ircConfig = {
-    // Either 'windows' or 'other'
-    os: os || 'windows',
+  // Either 'windows' or 'other'
+  os: os || "windows",
 
-    // Title of the window of the program
-    // Ex: 'Desmume' or 'VBA'
-    programName: programName || 'VBA',
+  // Title of the window of the program
+  // Ex: 'Desmume' or 'VBA'
+  programName: programName || "VBA",
 
-    // Ex: irc.twitch.tv or 199.9.252.26
-    server: serverIP || 'irc-ws.chat.twitch.tv',
-    // Your twitch username
-    nick: username,
-    // oauth token from www.twitchapps.com/tmi
-    password: oauth,
-    // name of channel
-    channel: channel,
+  // Ex: irc.twitch.tv or 199.9.252.26
+  server: serverIP || "irc-ws.chat.twitch.tv",
+  // Your twitch username
+  nick: username,
+  // oauth token from www.twitchapps.com/tmi
+  password: oauth,
+  // name of channel
+  channel: channel,
 
-    // If you want to print usernames/commands like in twitchplayspokemon
-    printToConsole: true,
-    // Maximum characters to show for a person's name in the console log
-    maxCharName: maxCharName || 8,
-    // Maximum characters to show for a command in the console log
-    // Ex: left => left since only 4 char, democracy => democra
-    maxCharCommand: maxCharCommand || 10,
+  // If you want to print usernames/commands like in twitchplayspokemon
+  printToConsole: true,
+  // Maximum characters to show for a person's name in the console log
+  maxCharName: maxCharName || 8,
+  // Maximum characters to show for a command in the console log
+  // Ex: left => left since only 4 char, democracy => democra
+  maxCharCommand: maxCharCommand || 10,
 
-    // If you need to filter the commands sent to the program
-    // Ex: democracy/anarchy since they don't affect the program itself
-    // Ex: ["democracy","anarchy"]
-    filteredCommands: filteredCommands || [],
+  // If you need to filter the commands sent to the program
+  // Ex: democracy/anarchy since they don't affect the program itself
+  // Ex: ["democracy","anarchy"]
+  filteredCommands: filteredCommands || [],
 
-    // If you want to prevent people from using from command too often
-    // Ex: ["start"]
-    throttledCommands: throttledCommands || [],
-    // Throttle time in seconds
-    // Ex: you can limit 'start' so it's only used every 10 sec
-    timeToWait: 10000,
+  // If you want to prevent people from using from command too often
+  // Ex: ["start"]
+  throttledCommands: throttledCommands || [],
+  // Throttle time in seconds
+  // Ex: you can limit 'start' so it's only used every 10 sec
+  timeToWait: 10000,
 
-    // Linux: delay between each possible keypress in ms (can't be too fast)
-    // If you want to change delay for windows - change key.py
-    delay: 100,
+  // Linux: delay between each possible keypress in ms (can't be too fast)
+  // If you want to change delay for windows - change key.py
+  delay: 100,
 
-    sendKey: sendKey,
-    commands: commands
+  sendKey: sendKey,
+  commands: commands,
 };
 
 module.exports = ircConfig;
--- app/server.js
+++ app/server.js
@@ -1,28 +1,28 @@
-var tmi = require('tmi.js'),
-printf = require('printf'),
-keyHandler = require('./keyHandler.js'),
-config = require('./config.js');
+var tmi = require("tmi.js"),
+  printf = require("printf"),
+  keyHandler = require("./keyHandler.js"),
+  config = require("./config.js");
 
 var client = new tmi.client(
-{
+  {
     options: {
-        debug: false
+      debug: false,
     },
     connection: {
-        server: config.server,
-        port: config.port || 443,
-        secure: true,
-        reconnect: true
+      server: config.server,
+      port: config.port || 443,
+      secure: true,
+      reconnect: true,
     },
     identity: {
-        username: config.nick,
-        password: config.password
+      username: config.nick,
+      password: config.password,
     },
-    channels: [config.channel]
-}
+    channels: [config.channel],
+  }
 
-// TODO: These configurations cannot fit inside tmi.js
-/*{
+  // TODO: These configurations cannot fit inside tmi.js
+  /*{
     sasl: false,
     floodProtection: config.floodProtection || false,
     floodProtectionDelay: config.floodProtectionDelay || 100
@@ -30,44 +30,46 @@ var client = new tmi.client(
 */
 );
 
-var commandRegex = config.regexCommands ||
-new RegExp('^(' + config.commands.join('|') + ')$', 'i');
+var commandRegex =
+  config.regexCommands ||
+  new RegExp("^(" + config.commands.join("|") + ")$", "i");
 
-client.on('message', function(channel, tags, message, self) {
-    if (self) return;
-    if (`#${config.channel}` === channel && message.match(commandRegex)) {
-        if (config.printToConsole) {
-            //format console output if needed
-            var from = tags.username;
-            var maxName = config.maxCharName,
-            maxCommand = config.maxCharCommand,
-            logFrom = from.substring(0, maxName),
-            logMessage = message.substring(0, 6).toLowerCase();
-            //format log
-            console.log(printf('%-' + maxName + 's % ' + maxCommand + 's',
-                logFrom, logMessage));
-        }
-
-        // Should the message be sent the program?
-        if (config.sendKey) {
-            keyHandler.sendKey(message.toLowerCase());
-        }
+client.on("message", function (channel, tags, message, self) {
+  if (self) return;
+  if (`#${config.channel}` === channel && message.match(commandRegex)) {
+    if (config.printToConsole) {
+      //format console output if needed
+      var from = tags.username;
+      var maxName = config.maxCharName,
+        maxCommand = config.maxCharCommand,
+        logFrom = from.substring(0, maxName),
+        logMessage = message.substring(0, 6).toLowerCase();
+      //format log
+      console.log(
+        printf("%-" + maxName + "s % " + maxCommand + "s", logFrom, logMessage)
+      );
     }
+
+    // Should the message be sent the program?
+    if (config.sendKey) {
+      keyHandler.sendKey(message.toLowerCase());
+    }
+  }
 });
 
-client.addListener('connected', function(address, port) {
-    console.log('Connected into the server!');
+client.addListener("connected", function (address, port) {
+  console.log("Connected into the server!");
 });
 
-client.addListener('logon', function() {
-    console.log('Logged into the server!');
+client.addListener("logon", function () {
+  console.log("Logged into the server!");
 });
 
 client.addListener("disconnected", function (reason) {
-    console.log('Disconnected from the server! Reason: ' + reason);
+  console.log("Disconnected from the server! Reason: " + reason);
 });
 
 client.connect();
-console.log('Connecting...');
-console.log(config.server + ': ' + config.nick);
-console.log('Channel: ' + config.channel + ", as username: " + config.nick);
+console.log("Connecting...");
+console.log(config.server + ": " + config.nick);
+console.log("Channel: " + config.channel + ", as username: " + config.nick);
